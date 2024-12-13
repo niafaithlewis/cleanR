4e09bc68dc5c677a441d33c304300c11e5568875
niafaithlewis <144560353+niafaithlewis@users.noreply.github.com>
2024-12-12 22:40:49 -0500

    Update DATASET.R

--- cleanR/data-raw/DATASET.R
+++ cleanR/data-raw/DATASET.R
@@ -5,6 +5,6 @@ plants_test <- read.csv("data-raw/plants_test.csv")
 
 plants_clean <- plants_test
 
-save(plants_clean, file = "cleanR/data/plants_clean.rda")
+save(plants_clean, file = "data/plants_clean.rda")
 
 usethis::use_data(plants_clean, overwrite = TRUE)
